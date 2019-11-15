fb75dc1921aa2eeada291547af5d6082924e6daf
mohamed <ragabmohame6@gmail.com>
2019-11-15 19:40:10 +0200

    new project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import TodoList from './TodoList';
-import { Link, Route, Redirect } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
         <div className="App">
             <h1>See Our Todos!</h1>
-            <p>
-                <Link to="/todos">See my todos!</Link>
-            </p>
-            <p>
-                <Link to="/todos/new">Add my todos!</Link>
-            </p>
-            <Route path="/todos" component={TodoList} />
-            <Route exact path="/" render={() => <Redirect to="/todos" />} />
         </div>
     );
   }
