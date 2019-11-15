4e37010a8dd739a05c043252fd9d998fbd864993
mohamed <ragabmohame6@gmail.com>
2019-11-15 19:34:51 +0200

    react-with-redux

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+import TodoList from './TodoList';
+import { Link, Route, Redirect } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+            <h1>See Our Todos!</h1>
+            <p>
+                <Link to="/todos">See my todos!</Link>
+            </p>
+            <p>
+                <Link to="/todos/new">Add my todos!</Link>
+            </p>
+            <Route path="/todos" component={TodoList} />
+            <Route exact path="/" render={() => <Redirect to="/todos" />} />
+        </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import rootReducer from './rootReducer';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+const store = createStore(rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
